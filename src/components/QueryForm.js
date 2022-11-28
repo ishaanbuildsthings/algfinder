7372ed00facd2e06eabe76920547545646c89896
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2022-11-28 01:58:59 -0500

    create intial basic frontend

---
+++ src/components/QueryForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function QueryForm() {
+
+    // initialize state
+    const [queries, setQueries] = React.useState({
+        scramble: "",
+        moveset: "",
+        depth: ""
+    });
+
+
+    // changes the object state when text is modified
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setQueries({
+            ...queries,
+            [name]: value
+        })
+    }
+
+
+    // generates the request URL
+    function handleClick() {
+        const {scramble, moveset, depth} = queries;
+        console.log(`http://127.0.0.1:5000/solve?scramble=${scramble}&max_depth=${depth}&move_types=${moveset}`);
+    }
+
+
+    // defines component
+    return (
+        <section className="queryForm">
+
+            <input
+                type="text"
+                placeholder="Enter Scramble Here"
+                className="form--scramble"
+                name="scramble"
+                value={queries.scramble}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                placeholder="Enter Allowed Moveset Here"
+                className="form--moveset"
+                name="moveset"
+                value={queries.moveset}
+                onChange={handleChange}
+            />
+            <input
+                type="text"
+                placeholder="Enter Max Algorithm Depth Here"
+                className="form--depth"
+                name="depth"
+                value={queries.depth}
+                onChange={handleChange}
+            />
+
+            <button
+                className="form--button"
+                onClick={handleClick}
+            >
+                Solve!
+            </button>
+
+        </section>
+    );
+}
+
+export default QueryForm;
