e3d6f907853633d1b8d718afcd835ba9d0d9f2c6
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-01-05 11:16:08 -0500

    refactor css properties alphabetically

--- src/Components/SolutionsDisplayContainer/SolutionsDisplayContainer.js
+++ src/Components/SolutionsDisplayContainer/SolutionsDisplayContainer.js
@@ -3,7 +3,6 @@ import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import Solution from '../Solution/Solution';
 import { useState, useEffect, useCallback } from 'react';
 import './SolutionsDisplayContainer.css';
-import { memo } from 'react';
 
 // * helpers
 // takes in an input of ["solution 1", "solution 2" ...]
@@ -111,4 +110,4 @@ function SolutionsDisplayContainer({ solutionsList }) {
 }
 
 export default SolutionsDisplayContainer;
-//export default memo(SolutionsDisplayContainer); // never re-renders unless the prop (a state, which does not change upon re-render of the parent) changes
+//TODO: export default memo(SolutionsDisplayContainer); // never re-renders unless the prop (a state, which does not change upon re-render of the parent) changes
